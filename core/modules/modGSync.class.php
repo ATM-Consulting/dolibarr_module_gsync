a02688746be67fdb14317d44eabe65e6a6f6fda2
atm-ph <phf@atm-consulting.fr>
2020-01-21 09:34:29 +0100

    Update num module

--- core/modules/modGSync.class.php
+++ core/modules/modGSync.class.php
@@ -47,7 +47,7 @@ class modGSync extends DolibarrModules
 		
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
-		$this->numero = 999999; // 104000 to 104999 for ATM CONSULTING
+		$this->numero = 104029; // 104000 to 104999 for ATM CONSULTING
 		// Key text used to identify module (for permissions, menus, etc...)
 		$this->rights_class = 'gsync';
 
